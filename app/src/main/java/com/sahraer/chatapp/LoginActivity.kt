d0b7aef410198f1ef9e836c0c7973cca1d0ebd2b
Sahranur Er <sahranurerr@gmail.com>
2021-10-18 18:27:59 +0300

    ui design and login transactions done

--- app/src/main/java/com/sahraer/chatapp/LoginActivity.kt
+++ app/src/main/java/com/sahraer/chatapp/LoginActivity.kt
@@ -2,10 +2,63 @@ package com.sahraer.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.sahraer.chatapp.databinding.ActivityLoginBinding
+import com.sahraer.chatapp.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+         binding = ActivityLoginBinding.inflate(layoutInflater)
+         val view = binding.root
+         setContentView(view)
+         binding.buttonLogin.setOnClickListener {
+          performLogin()
+        }
+
+        binding.backToRegister.setOnClickListener {
+            finish()
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+    private fun performLogin(){
+        val email = binding.emailEdittextLogin.text.toString()
+        val password = binding.passwordEdittextLogin.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this,"Please enter text in email/password ",Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        auth = Firebase.auth
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener{ task ->
+                if (!task.isSuccessful) return@addOnCompleteListener
+                Log.d("LoginActivity", "Successfuly login user with uid: ${task.result?.user?.uid}")
+            }
+            .addOnFailureListener {
+                Log.d("LoginActivity","Failed to login user: ${it.message}")
+            }
     }
 }
--- app/src/main/java/com/sahraer/chatapp/MainActivity.kt
+++ app/src/main/java/com/sahraer/chatapp/MainActivity.kt
@@ -1,37 +1,79 @@
 package com.sahraer.chatapp
 
+import android.Manifest
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.sahraer.chatapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    val username_edittext_register by lazy {findViewById<EditText>( R.id.username_edittext_register) }
-    val email_edittext_register by lazy { findViewById<EditText>(R.id.email_edittext_register) }
-    val password_edittext_register by lazy { findViewById<EditText>(R.id.password_edittext_register) }
-    val register_button by lazy {findViewById<Button>(R.id.button_register)}
-    val already_have_account_textView by lazy {findViewById<TextView>(R.id.already_have_account_textView)}
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-        register_button.setOnClickListener {
-            Log.d("MainActivity", "Email is : "+email_edittext_register.text.toString())
-            Log.d("MainActivity","Password is : "+password_edittext_register.text.toString())
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.buttonRegister.setOnClickListener {
+            performRegister()
         }
 
-        already_have_account_textView.setOnClickListener {
-            Log.d("MainActivity","Try to show login activity ")
-            val intent = Intent(this@MainActivity,LoginActivity::class.java)
+        binding.alreadyHaveAccountTextView.setOnClickListener {
+            Log.d("MainActivity", "Try to show login activity ")
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        binding.imageViewSelect.setOnClickListener {
 
 
 
     }
+
+
+
+    }
+
+
+
+    private fun performRegister(){
+        val email = binding.emailEdittextRegister.text.toString()
+        val password = binding.passwordEdittextRegister.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this,"Please enter text in email/password ",Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (!task.isSuccessful) return@addOnCompleteListener
+
+            // If sign in fails, display a message to the user.
+            Log.d("MainActivity", "Successfuly created user with uid: ${task.result?.user?.uid}")
+            }
+            .addOnFailureListener {
+                Log.d("MainActivity","Failed to create user: ${it.message}")
+                Toast.makeText(this,"Failed to create user: ${it.message}",Toast.LENGTH_SHORT).show()
+            }
+
+    }
+
+
 }
+
